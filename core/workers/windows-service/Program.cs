0f341262fa473f90cbb67bed0872843e5f618bf1
박성원 <skfhddlg@naver.com>
2023-03-21 12:26:50 +0900

    윈도우 서비스 설치용 프로젝트만 남김, 튜토리얼 추가 작업사항 반영 : https://learn.microsoft.com/dotnet/core/extensions/windows-service-with-installer

--- core/workers/windows-service/Program.cs
+++ core/workers/windows-service/Program.cs
@@ -1,6 +1,34 @@
 ﻿using App.WindowsService;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
+using CliWrap;
+
+const string ServiceName = ".NET Joke Service";
+
+if (args is { Length: 1 })
+{
+    string executablePath =
+        Path.Combine(AppContext.BaseDirectory, "App.WindowsService.exe");
+
+    if (args[0] is "/Install")
+    {
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
+            .ExecuteAsync();
+    }
+    else if (args[0] is "/Uninstall")
+    {
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "stop", ServiceName })
+            .ExecuteAsync();
+
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "delete", ServiceName })
+            .ExecuteAsync();
+    }
+
+    return;
+}
 
 using IHost host = Host.CreateDefaultBuilder(args)
     .UseWindowsService(options =>
